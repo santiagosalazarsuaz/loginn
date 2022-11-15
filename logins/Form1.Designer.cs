de7546638982a2c36ad5bb70027aa7f7f36c25bb
santiagosalazarsuaz <113456937+santiagosalazarsuaz@users.noreply.github.com>
2022-11-14 22:58:09 -0500

    Add files via upload

---
+++ logins/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace logins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnregistro = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(267, 67);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(147, 22);
+            this.txtusuario.TabIndex = 0;
+            this.txtusuario.TextChanged += new System.EventHandler(this.txtusuario_TextChanged);
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Location = new System.Drawing.Point(267, 199);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.PasswordChar = '0';
+            this.txtcontraseña.Size = new System.Drawing.Size(147, 22);
+            this.txtcontraseña.TabIndex = 1;
+            this.txtcontraseña.TextChanged += new System.EventHandler(this.txtcontraseña_TextChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(299, 323);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnregistro
+            // 
+            this.btnregistro.Location = new System.Drawing.Point(569, 323);
+            this.btnregistro.Name = "btnregistro";
+            this.btnregistro.Size = new System.Drawing.Size(190, 23);
+            this.btnregistro.TabIndex = 3;
+            this.btnregistro.Text = "registrarse como alumno";
+            this.btnregistro.UseVisualStyleBackColor = true;
+            this.btnregistro.Click += new System.EventHandler(this.btnregistro_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-3, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "registrarse como profesor";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnregistro);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtusuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnregistro;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ logins/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logins
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection coneccion = new SqlConnection("server=DESKTOP-SDSD7PR\\DORITO; database = login; integrated security = true");
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            coneccion.Open();
+            SqlCommand comando = new SqlCommand("select usuario, contraseña from pal  where usuario = @vusuario and contraseña = @vcontraseña",coneccion);
+            comando.Parameters.AddWithValue("@vusuario", txtusuario.Text);
+            comando.Parameters.AddWithValue("@vcontraseña", txtcontraseña.Text);
+             
+
+
+            SqlDataReader lector = comando.ExecuteReader();
+            if (lector.Read())
+            {
+                formulario pantalla = new formulario();
+                pantalla.Show();
+            } 
+        }
+
+        private void txtusuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcontraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnregistro_Click(object sender, EventArgs e)
+        {
+           registro lupa = new registro();
+           lupa.Show();
+        }
+    }
+}
